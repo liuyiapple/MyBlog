b959c90992c20a53c6b6655b3fd15d34e8dfa390
liuyao <yi7305395@gmail.com>
2023-07-27 08:44:02 +0800

    fix:icon

--- docs/Html/html.md
+++ docs/Html/html.md
@@ -3,3 +3,9 @@
 ::: tip 暂时先别看这块，我还没写
 我还没开始写这里，所以，看看后面的
 :::
+
+## iframe
+
+<script setup>
+import ifram from "./iframe/index.vue";
+</script>
---
+++ docs/Html/iframe/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="content">
+    <iframe
+      src="https://cn.vuejs.org/"
+      frameborder="0"
+      class="iframe-content"
+    />
+  </div>
+</template>
+<script lang="ts" setup></script>
+<style lang="less" scoped>
+.content {
+  border: 1px dotted #ccc;
+}
+.iframe-content {
+  width: 100%;
+  height: 100vh;
+}
+</style>
